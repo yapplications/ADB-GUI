d096cadd6c5cbb413b5f6195a5afac10c93f2663
Evgeni Shafran <evgeni.shafran@yapplications.com>
2017-01-21 23:19:29 +1100

    0.1.7v

--- src/application/preferences/PreferenceController.java
+++ src/application/preferences/PreferenceController.java
@@ -75,7 +75,7 @@ public class PreferenceController implements Initializable{
 
 	private String getVersion() {
 		Package p = getClass().getPackage();
-		return "0.1.6v";// p.getImplementationVersion();
+		return "0.1.7v";// p.getImplementationVersion();
 	}
 
 	public void handleYapplicationsLink(ActionEvent actionEvent) {
